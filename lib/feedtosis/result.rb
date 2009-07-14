06ee4fa7785159c999ad7263b40aed68048f03d5
Justin S. Leitgeb <justin@phq.org>
2009-07-14 18:00:58 -0400

    Cleanup in result class

--- lib/feedtosis/result.rb
+++ lib/feedtosis/result.rb
@@ -7,17 +7,8 @@ module Feedtosis
   # these cases, depending on your business logic you may want to inspect the 
   # state of the Curl::Easy object by using methods forwarded to it.
   class Result
-    
-    # Methods which should be delegated to the FeedNormalizer::Feed object.
-    FEED_METHODS = [ :title, :description, :last_updated, :copyright, :authors, 
-      :author, :urls, :url, :image, :generator, :items, :entries, :new_items, 
-      :new_entries, :channel, :ttl, :skip_hours, :skip_days 
-    ] unless defined?(FEED_METHODS)
-    
-    # Precompiled regexp for detecting removing setter methods from collection 
-    # of methods to be delegated to the Curl::Easy object.
-    SETTER_METHOD_RE = /=$/o unless defined?(SETTER_METHOD_RE)
-    
+    extend Forwardable
+        
     def initialize(curl, feed)
       @curl = curl
       @feed = feed
@@ -25,22 +16,17 @@ module Feedtosis
       raise ArgumentError, "Curl object must not be nil" if curl.nil?
     end
     
-    # See what the Curl::Easy object responds to, and send any appropriate
-    # messages its way.  We don't worry about setter methods, since those
-    # aren't really useful to delegate.
-    Curl::Easy.instance_methods(false).reject do |m| 
-      m =~ SETTER_METHOD_RE
-    end.each do |meth|                  # Example method:
-      define_method meth do |*args|     # def title
-        @curl.__send__(meth, *args)     #   @curl.title
-      end                               # end
-    end
+    # See what the Curl::Easy object responds to, and send any appropriate messages its way.  We ignore  
+    # Curl setter methods since those aren't really useful to delegate.
+    def_delegators :@curl, *Curl::Easy.instance_methods(false).reject {|m| m =~ /=$/o }
     
-    # Send methods through to the feed object unless it is nil.  If feed
-    # object is nil, return nil in response to method call.
-    FEED_METHODS.each do |meth|                         # Example method:
+    # Send methods through to the feed object unless it is nil.  If feed object is nil, return nil in response to method call.
+    # Unfortunately we can't just see what the object responds to, since FeedNormalizer uses method_missing.
+    [ :title, :description, :last_updated, :copyright, :authors, :author, :urls, :url, :image, :generator, :items, 
+      :entries, :new_items, :new_entries, :channel, :ttl, :skip_hours, :skip_days 
+    ].each do |meth|                                    # Example method:
       define_method meth do |*args|                     # def author
-        @feed.nil? ? nil : @feed.__send__(meth, *args)  #   @feed.nil? nil : @feed.author
+        @feed.__send__(meth, *args) unless @feed.nil?   #   @feed.author unless @feed.nil?
       end                                               # end
     end    
     
